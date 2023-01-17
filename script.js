3e1fe4efe54ae2ab6428319623aa5dd7d077a920
GuiSegatto <guisegatto23@gmail.com>
2023-01-17 19:45:11 -0300

    added banner badkgrounds, trailers, gifs

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
+const bannerGameSlider = document.querySelectorAll('input[name="slider"]')
+const gameTrailer = document.querySelector('.trailer')
 
-
-
+/* on load */
 disableSliderGames()
+animateTrailer('diablo')
+bannerGameSlider.forEach(item => item.addEventListener('change', changeBannerInfo))
+
+
+
+/* FUNCTIONS */
+function changeBannerInfo() {
+    let selectedGame = this.id
+    animateTrailer(selectedGame);
+    setBannerImages(selectedGame);
+    setBannerText(selectedGame);
+};
+
 function disableSliderGames() {
     const disabledGamesID = ['#starCraftII', '#diabloI']
     document.querySelectorAll(disabledGamesID).forEach(game => game.disabled = true)
 }
 
+function setBannerImages(game) {
+    const wowOverlay = document.querySelector('.wow-overlay')
+    const gameBackgroundOverlay = document.querySelector('.background-overlay')
+    const gameBackground = document.querySelector('.background-banner');
+    const gameLogo = document.querySelector('.game-logo')
 
-const bannerGameSlider = document.querySelectorAll('input[name="slider"]')
-bannerGameSlider.forEach(item => item.addEventListener('change', changeBannerInfo))
-function changeBannerInfo() {
+    gameBackground.style.backgroundImage = `url(/assets/banner-hero/games/${game}-bg.png)`;
+    gameLogo.style.backgroundImage = `url(/assets/banner-hero/games/${game}-logo.png)`;
+    gameTrailer.style.backgroundImage = `url(/assets/banner-hero/games/${game}-animation-cover.png)`;
+    if (game === 'wow') {
+        gameBackgroundOverlay.style.backgroundImage = `url(/assets/banner-hero/games/${game}-bg-overlay.png)`;
+        wowOverlay.style.display = 'block'
+    } else {
+        wowOverlay.style.display = ''
+        gameBackgroundOverlay.style.backgroundImage = `url(/assets/banner-hero/games/${game}-bg-overlay.png)`;
+    }
+
+}
+
+function setBannerText(game) {
     const gameHeaderText = document.querySelector('.game-info h1');
     const gameText = document.querySelector('.game-info p');
     const gameButton = document.querySelector('.game-info button');
-    const gameBackground = document.querySelector('.background-banner');
-    const gameLogo = document.querySelector('.game-logo')
-    const gameTrailer = document.querySelector('.trailer')
-
     const PLAY_NOW = 'Jogue Agora'
     const PRE_ORDER = 'Reserve agora na pré-venda'
 
@@ -28,31 +54,27 @@ function changeBannerInfo() {
             button: PLAY_NOW,
         },
         hearthstone: {
-            header:'Novo pacote de Expansão de Hearthstone',
-            text:'A horda e aliança se encontram no vale alterac para lutar',
-            button:PRE_ORDER,
+            header: 'Novo pacote de Expansão de Hearthstone',
+            text: 'A horda e aliança se encontram no vale alterac para lutar',
+            button: PRE_ORDER,
         },
         wow: {
-            header:'Desbrave as Terras Sombrias em Shadowlands!',
-            text:'O que jaz além do mundo que você conhece?',
-            button:PRE_ORDER,
+            header: 'Desbrave as Terras Sombrias em Shadowlands!',
+            text: 'O que jaz além do mundo que você conhece?',
+            button: PRE_ORDER,
         },
     }
 
-    let selectedGame = this.id
-    gameHeaderText.textContent = gameInfo[selectedGame].header
-    gameText.textContent = gameInfo[selectedGame].text
-    gameButton.textContent = gameInfo[selectedGame].button
+    gameHeaderText.textContent = gameInfo[game].header
+    gameText.textContent = gameInfo[game].text
+    gameButton.textContent = gameInfo[game].button
+}
 
-    gameBackground.style.backgroundImage = `url(/assets/banner-hero/games/${selectedGame}-bg.png)`;
-    gameLogo.style.backgroundImage = `url(/assets/banner-hero/games/${selectedGame}-logo.png)`;
-    gameTrailer.style.backgroundImage = `url(/assets/banner-hero/games/${selectedGame}-animation-cover.png)`;
+function animateTrailer(game) {
     gameTrailer.addEventListener('mouseover', () => {
-        gameTrailer.style.backgroundImage = `url(/assets/banner-hero/games/${selectedGame}-animation.gif)`;
+        gameTrailer.style.backgroundImage = `url(/assets/banner-hero/games/${game}-animation.gif)`;
     });
     gameTrailer.addEventListener('mouseout', () => {
-        gameTrailer.style.backgroundImage = `url(/assets/banner-hero/games/${selectedGame}-animation-cover.png)`;
+        gameTrailer.style.backgroundImage = `url(/assets/banner-hero/games/${game}-animation-cover.png)`;
     });
-};
-
-
+}
